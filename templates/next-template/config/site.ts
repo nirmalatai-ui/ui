4ea7fcad68ffa05792bcbe07833f9be428acf3cd
Ekjot Singh <ekjotmakhija@gmail.com>
2025-01-04 00:43:25 +0530

    fix: run templates on next js

---
+++ templates/next-template/config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Next.js",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/Ekjotmakhija",
+    github: "https://github.com/nirmalatai-ui/ui",
+    docs: "https://ui.nirmalatai.com",
+  },
+}
