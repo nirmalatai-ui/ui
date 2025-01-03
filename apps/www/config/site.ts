b7a50bc6013b8c3ca1ab3f30284e6dba3e003481
Ekjot Singh <ekjotmakhija@gmail.com>
2025-01-04 01:17:05 +0530

    feat: add www aka web-ui for the project

---
+++ apps/www/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "nirmalatai/ui",
+  url: "https://ui.nirmalatai.com",
+  ogImage: "https://ui.nirmalatai.com/og.jpg",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  links: {
+    twitter: "https://twitter.com/Ekjotmakhija",
+    github: "https://github.com/nirmalatai-ui/ui",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
